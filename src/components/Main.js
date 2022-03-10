c2352d72a8a80d775b381b9378604fcf06b667e4
Bruno Minelli <minellibruno@hotmail.com>
2022-03-10 18:32:04 -0300

    Crie a página do carrinho de compras

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Main extends Component {
   render() {
     return (
       <main className="container-data">
         <input type="text" name="search" />
+        <Link to="/shopping-cart" data-testid="shopping-cart-button">Meu carrinho</Link>
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
